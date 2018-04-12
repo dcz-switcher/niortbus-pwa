4eccf3d4be8e878ead2e8c23925da5d1f8adf231
David CHOLLEZ <davidc@MacBook-Pro-de-David.local>
2018-04-12 21:50:21 +0200

    home view

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import LineButton from './components/LineButton';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <h4>Niort bus</h4>
+                <h1>Lignes TAN</h1>
+            </header>
+            <div className="App-body">
+                <LineButton start="Trévin / parpin" end="Pôle Universitaire" number="1"/>
+                <LineButton start="Ebaupin / Bois Chamaillard" end="Brizeaux CAF" number="2"/>
+                <LineButton start="Pôle Universitaire" end="Terre de sport" number="3"/>
+                <LineButton start="Mairie Aiffre" end="Sainte Pezenne" number="4"/>
+                <LineButton start="Parpin / château Driguet" end="Chaintre brûlée" number="5"/>
+            </div>
+        </div>
     );
   }
 }
