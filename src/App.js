22ce8ef118ae3100beada3f0cf9cc45cb561aac7
David CHOLLEZ <david.chollez@gmail.com>
2018-04-13 12:37:46 +0200

    add detail view

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import HomeView from './views/HomeView';
+import DetailView from './views/DetailView';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
             <HomeView/>
+            <DetailView/>
         </div>
     );
   }
---
+++ src/views/DetailView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './DetailView.css';
+
+const DetailView = () => (
+    <div className="Detail-view">
+        <h4>Niort bus</h4>
+        <h1>Detail View</h1>
+    </div>
+);
+
+export default DetailView;
