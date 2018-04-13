b45279e89366b6404583a9fb32db3f3f8985401d
David CHOLLEZ <david.chollez@gmail.com>
2018-04-13 12:01:19 +0200

    import and use new HomeView

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-import LineButton from './components/LineButton';
-
 import HomeView from './views/HomeView';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-            <header className="App-header">
-                <h4>Niort bus</h4>
-                <h1>Lignes TAN</h1>
-            </header>
-            <div className="App-body">
-                <LineButton start="Trévin / parpin" end="Pôle Universitaire" number="1"/>
-                <LineButton start="Ebaupin / Bois Chamaillard" end="Brizeaux CAF" number="2"/>
-                <LineButton start="Pôle Universitaire" end="Terre de sport" number="3"/>
-                <LineButton start="Mairie Aiffre" end="Sainte Pezenne" number="4"/>
-                <LineButton start="Parpin / château Driguet" end="Chaintre brûlée" number="5"/>
-            </div>
+            <HomeView/>
         </div>
     );
   }
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import './HomeView.css';
+
+import LineButton from './../components/LineButton';
+
 const HomeView = () => (
-    <div>
-        <h1>Home view</h1>
+    <div className="Home-view">
+        <header className="Home-header">
+            <h4>Niort bus</h4>
+            <h1>Lignes TAN</h1>
+        </header>
+        <div className="Home-body">
+            <LineButton start="Trévin / parpin" end="Pôle Universitaire" number="1"/>
+            <LineButton start="Ebaupin / Bois Chamaillard" end="Brizeaux CAF" number="2"/>
+            <LineButton start="Pôle Universitaire" end="Terre de sport" number="3"/>
+            <LineButton start="Mairie Aiffre" end="Sainte Pezenne" number="4"/>
+            <LineButton start="Parpin / château Driguet" end="Chaintre brûlée" number="5"/>
+        </div>
     </div>
 );
 
