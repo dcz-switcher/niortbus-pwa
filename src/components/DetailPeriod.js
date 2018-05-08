88f8053306025eb7cfa556c3b90a8f1c961eef9d
David CHOLLEZ <david.chollez@gmail.com>
2018-05-08 17:33:52 +0200

    styling period selector

--- src/components/DetailPeriod.js
+++ src/components/DetailPeriod.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import downArrow from './../assets/ic_keyboard_arrow_down_black_24px.svg';
+
+import './DetailPeriod.css';
+
 const DetailPeriod = () => (
-    <div>selection de la periode</div>
+    <div className="Period-selector" onClick={() => console.log('click')}>
+        <div className='Period-label'>periode</div>
+        <img src={downArrow} alt='' className='Down-arrow'/>
+    </div>
 );
 
 export default DetailPeriod;
