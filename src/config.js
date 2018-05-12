b0d2c51dbac5bb09a16d0b4dcf2dac3ba17bf361
David CHOLLEZ <david.chollez@gmail.com>
2018-05-12 14:55:58 +0200

    load lines 6 and 7

--- src/config.js
+++ src/config.js
@@ -5,6 +5,8 @@ const Config  = {
         {busId : '3', color: '#5EC4AE', start: 'Pôle Universitaire', end: 'Terre de sport', numberPosition: '74px -128px', data: require('./lines/ligne3.json')},
         {busId : '4', color: '#E78093', start: 'Mairie Aiffre', end: 'Sainte Pezenne', numberPosition: '74px -192px', data: require('./lines/ligne4.json')},
         {busId : '5', color: '#2C77BB', start: 'Parpin / château Driguet', end: 'Chaintre brûlée', numberPosition: '74px -256px', data: require('./lines/ligne5.json')},
+        {busId : '6', color: '#8CC63E', start: 'Surimeau', end: 'St Liguaire', numberPosition: '74px -320px', data: require('./lines/ligne6.json')},
+        {busId : '7', color: '#A4548B', start: 'Brizeaux CAF', end: 'Pied de fond / Montamisé', numberPosition: '74px -395px', data: require('./lines/ligne7.json')},
     ],
 }
 
