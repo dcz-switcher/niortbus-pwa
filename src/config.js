bd9e874cd5fa57604e7224e0dd7dd5d3caa36e10
David CHOLLEZ <dchollez@macif.fr>
2019-09-05 08:12:01 +0200

    update lines

--- src/config.js
+++ src/config.js
@@ -1,14 +1,15 @@
 const Config  = {
     lines : [
-        {busId : '1', color: '#ED1D25', start: 'Trévin / parpin', end: 'Pôle Universitaire', numberPosition: '74px 0', data: require('./lines/ligne1.json')},
-        {busId : '2', color: '#00854B', start: 'Ebaupin / Bois Chamaillard', end: 'Brizeaux CAF', numberPosition: '74px -64px', data: require('./lines/ligne2.json')},
+        {busId : '1', color: '#ED1D25', start: 'Université', end: 'Chauray', numberPosition: '74px 0', data: require('./lines/ligne1.json')},
+        {busId : '2', color: '#00854B', start: 'Bessines', end: 'Brizeaux CAF', numberPosition: '74px -64px', data: require('./lines/ligne2.json')},
         {busId : '3', color: '#5EC4AE', start: 'Pôle Universitaire', end: 'Terre de sport', numberPosition: '74px -128px', data: require('./lines/ligne3.json')},
-        {busId : '4', color: '#E78093', start: 'Mairie Aiffre', end: 'Sainte Pezenne', numberPosition: '74px -192px', data: require('./lines/ligne4.json')},
-        {busId : '5', color: '#2C77BB', start: 'Parpin / château Driguet', end: 'Chaintre brûlée', numberPosition: '74px -256px', data: require('./lines/ligne5.json')},
-        {busId : '6', color: '#8CC63E', start: 'Surimeau', end: 'St Liguaire', numberPosition: '74px -320px', data: require('./lines/ligne6.json')},
-        {busId : '7', color: '#A4548B', start: 'Brizeaux CAF', end: 'Pied de fond / Montamisé', numberPosition: '74px -395px', data: require('./lines/ligne7.json')},
-        {busId : '8', color: '#DE2370', start: 'Surimeau', end: 'Trente Ormeaux', numberPosition: '74px -470px', data: require('./lines/ligne8.json')},
+        {busId : '4', color: '#E78093', start: 'Sainte Pezenne', end: 'Aiffres', numberPosition: '74px -192px', data: require('./lines/ligne4.json')},
+        {busId : '5', color: '#2C77BB', start: 'Chaintre brûlée', end: 'Château Driguet', numberPosition: '74px -256px', data: require('./lines/ligne5.json')},
+        {busId : '6', color: '#8CC63E', start: 'St Liguaire', end: 'Coubertin', numberPosition: '74px -320px', data: require('./lines/ligne6.json')},
+        {busId : '7', color: '#A4548B', start: 'Pôle Atlantique', end: 'Maisons Rouges', numberPosition: '74px -395px', data: require('./lines/ligne7.json')},
+        {busId : '8', color: '#DE2370', start: 'Surimeau', end: 'Hôpital', numberPosition: '74px -470px', data: require('./lines/ligne8.json')},
         {busId : '9', color: '#F49719', start: 'Pôle Atlantique', end: 'Collège Curie', numberPosition: '74px -550px', data: require('./lines/ligne9.json')},
+        {busId : '10', color: '#793329', start: 'Cholette', end: 'Trente Ormeaux', numberPosition: '74px 0', data: require('./lines/ligne10.json')},
     ],
 }
 
