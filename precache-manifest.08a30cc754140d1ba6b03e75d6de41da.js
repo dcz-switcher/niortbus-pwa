335e3a7ce740d3bb6eaa22cd41094311fe41c7c1
David CHOLLEZ <dchollez@macif.fr>
2020-09-19 10:37:04 +0200

    Updates

---
+++ precache-manifest.08a30cc754140d1ba6b03e75d6de41da.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348c9c5edf21203df8d236df3e16ca43",
+    "url": "/niortbus-pwa/index.html"
+  },
+  {
+    "revision": "7668a0178da47137aa04",
+    "url": "/niortbus-pwa/static/css/main.d4a6d7de.chunk.css"
+  },
+  {
+    "revision": "7cb86f8e21e2010ef321",
+    "url": "/niortbus-pwa/static/js/2.f8436db5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/niortbus-pwa/static/js/2.f8436db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7668a0178da47137aa04",
+    "url": "/niortbus-pwa/static/js/main.5023f9be.chunk.js"
+  },
+  {
+    "revision": "dbfcf687720bf28bd0f7",
+    "url": "/niortbus-pwa/static/js/runtime-main.d4b4c9d5.js"
+  },
+  {
+    "revision": "7b66831ce156de757d9bc34d4744da93",
+    "url": "/niortbus-pwa/static/media/ic_close_white_24px.7b66831c.svg"
+  },
+  {
+    "revision": "1643e4c0ada2ae5eeabbbb5e7f484745",
+    "url": "/niortbus-pwa/static/media/ic_compare_arrows_white_24px.1643e4c0.svg"
+  },
+  {
+    "revision": "cd90a7abdac44b6d21cbf3c44852cbb4",
+    "url": "/niortbus-pwa/static/media/ic_keyboard_arrow_down_black_24px.cd90a7ab.svg"
+  },
+  {
+    "revision": "f9a6cf823392d8657eee1fc895df5f8e",
+    "url": "/niortbus-pwa/static/media/line-numbers.f9a6cf82.png"
+  }
+]);
